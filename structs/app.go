dc7c296aa65b94a5c1d9900a08559ba23b5d3887
Rengkat <successinmotion11@.com>
2025-04-09 15:10:39 +0100

    structs function

--- structs/app.go
+++ structs/app.go
@@ -7,13 +7,24 @@ type User struct {
 	age    int
 	height float64
 }
+
 // desc is a method on the User type
 func (u User) desc() {
 	fmt.Printf("My name is %s. I am %d years old and %.2f meters tall.\n", u.name, u.age, u.height)
 }
-func (u *User) clearName()  {
-	u.name=""
+
+func (u *User) clearName() {
+	u.name = ""
 }
+
+func newUser(name string, age int, height float64) User {
+	return User{
+		name:   name,
+		age:    age,
+		height: height,
+	}
+}
+
 func main() {
 	var name string
 	var age int
@@ -26,17 +37,11 @@ func main() {
 	fmt.Print("Enter height: ")
 	fmt.Scan(&height)
 
-	// Create a User instance with the input values
-	user := User{
-		name:   name,
-		age:    age,
-		height: height,
-	}
+	// Create a User instance using the newUser function
+	user := newUser(name, age, height)
 
 	// Call the desc method on the User instance
 	user.desc()
 	user.clearName()
 	user.desc()
-
 }
-
